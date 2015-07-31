8f04512749b23606564031e29277c4908ef54186
Ed Flanagan <edsync256@gmail.com>
2015-07-31 19:05:21 -0400

    Make with C++11 & use constexpr
    * Update CFLAGS to use C++11
    * Change earth radius values to constexpr
    * Add earth radius in meters and miles

--- src/geo_distance.hpp
+++ src/geo_distance.hpp
@@ -7,7 +7,9 @@
 #define M_PI    3.1415926535897932384626433832795
 #endif
 
-static const double earth_radius_km = 6371.0;
+static constexpr double earth_radius_km = 6371.0;
+static constexpr double earth_radius_m  = 6371000.0;
+static constexpr double earth_radius_mi = 3959.0;
 
 double deg_to_rad(double deg);
 double haversine_distance(double latitude1, double longitude1, double latitude2,
